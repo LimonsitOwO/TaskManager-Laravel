82dfbcc96e3bb60b4f8cfe8e3e161d23ea3ee45d
Luis Carlos <luislm080304@gmail.com>
2025-02-25 22:04:22 -0500

    Add: REST CRUD API

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+  public function createTask(Request $request)
+  {
+    $task = new Task; 
+  
+    $task->title = $request->title;
+    $task->description = $request->description;
+    $task->due_date = $request->due_date;
+    $task->status = $request->status;
+
+    $task->save();
+    return $task;
+  }
+
+  public function findAllTask(Request $request)
+  { 
+    return Task::all();
+  }
+
+  public function findTaskById(Request $request, $id)
+  {
+    return Task::find($id);
+  }
+
+  public function updateTask(Request $request, $id)
+  {
+    $task = Task::find($id);
+    $task->title = $request->title;
+    $task->description = $request->description;
+    $task->due_date = $request->due_date;
+    $task->status = $request->status;
+    $task->save();
+    return $task;
+  }
+
+  public function deleteTask(Request $request, $id)
+  {
+    $task = Task::find($id);
+    $task->delete();
+    return $task;
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/my-task', function () {
+  return view('task.my-task');
+});
+Route::get('/create-task', function () {
+  return view('task.create-task');
 });
